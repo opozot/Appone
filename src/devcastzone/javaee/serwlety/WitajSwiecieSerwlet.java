8de15d69a583f78b660c3fd5b5cee40dc16aad45
sbielaws <szymonbielawski@gmail.com>
2018-03-22 21:00:00 +0100

    taglibs in index.jsp

--- src/devcastzone/javaee/serwlety/WitajSwiecieSerwlet.java
+++ src/devcastzone/javaee/serwlety/WitajSwiecieSerwlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 
 public class WitajSwiecieSerwlet extends HttpServlet {
@@ -18,6 +19,11 @@ public class WitajSwiecieSerwlet extends HttpServlet {
         u.setImie( "Jan" );
         u.setNazwisko( "Kowalski" );
         req.setAttribute( "uzytkownik", u );
+        Uzytkownik drugi = new Uzytkownik();
+        drugi.setId( 2 );
+        drugi.setImie( "Andrzej" );
+        drugi.setNazwisko( "Nowak" );
+        req.setAttribute( "lista", Arrays.asList( u, drugi ) );
         RequestDispatcher rd = req.getRequestDispatcher( "/index.jsp" );
         rd.forward( req, res );
     }
