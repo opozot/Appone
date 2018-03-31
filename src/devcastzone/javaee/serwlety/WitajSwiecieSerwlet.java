ce32a3ac176260ca5c385a7c5e31f2b442f74027
sbielaws <szymonbielawski@gmail.com>
2018-03-31 18:25:19 +0200

    Added DB & db connection

--- src/devcastzone/javaee/serwlety/WitajSwiecieSerwlet.java
+++ src/devcastzone/javaee/serwlety/WitajSwiecieSerwlet.java
@@ -1,31 +1,29 @@
 package devcastzone.javaee.serwlety;
 
-import devcastzone.javaee.Uzytkownik;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
+import java.sql.*;
 
 
 public class WitajSwiecieSerwlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        Uzytkownik u = new Uzytkownik();
-        u.setId( 1 );
-        u.setImie( "Jan" );
-        u.setNazwisko( "Kowalski" );
-        req.setAttribute( "uzytkownik", u );
-        Uzytkownik drugi = new Uzytkownik();
-        drugi.setId( 2 );
-        drugi.setImie( "Andrzej" );
-        drugi.setNazwisko( "Nowak" );
-        req.setAttribute( "lista", Arrays.asList( u, drugi ) );
-        RequestDispatcher rd = req.getRequestDispatcher( "/index.jsp" );
-        rd.forward( req, res );
+        try {
+            Driver sterownik = new com.mysql.jdbc.Driver();
+            DriverManager.registerDriver( sterownik );
+            Connection conn = sterownik.connect( "jdbc:mysql://localhost/szkolenie_javaee?user=root&amp;password=root&characterEncoding=utf8", null );
+            PreparedStatement stmt = conn.prepareStatement( "INSERT INTO uzytkownik VALUES (NULL, ?, ?)" );
+            stmt.setString( 1, "Marcin" );
+            stmt.setString( 2, "Kowalski" );
+            if (stmt.executeUpdate() > 0)
+                res.getWriter().println( "Rekord został dodany" );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
--- web/index.jsp
+++ web/index.jsp
@@ -1,14 +1,14 @@
 <%-- Created by IntelliJ IDEA. --%>
 <%@ page contentType="text/html;charset=UTF-8" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
 <html>
 <head>
     <title>Appone</title>
 </head>
 <body>
 <p>Hello World!</p>
-<c:forEach items="${lista}" var="u">
-    ${u.imie} ${u.nazwisko} <br/>
-</c:forEach>
+
 </body>
 </html>
